87a00554a1117964e4d948503d14341930a0aad8
nqtran1 <nqtran@ucsd.edu>
2021-02-25 10:47:21 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = '0g7cNFSH14Hgq7p28aPltgiXC9CwKGA3';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [36.115815856537935, -115.17340760247174],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+});
+
+L.marker([36.115815856537935, -115.17340760247174]).addTo(map);
 }
